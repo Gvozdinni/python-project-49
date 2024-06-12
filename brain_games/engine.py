a88c246f54ff90ce6397d801f6a4b9ab0c11d34d
“VG” <lolmanip201289@gmail.com>
2024-06-12 17:56:23 +0400

    Correction

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,11 +1,12 @@
-from brain_games import cli
+import prompt
+NUMBER_OF_GAME = 3
 
 
 def start_game(game):
-    name = cli.welcome_user()
+    name = get_name()
     print(game.EXERCISE)
     x = 0
-    while x < 3:
+    while x < NUMBER_OF_GAME:
         question, correct_answer = game.generate_question()
         print(f'Question: {question}')
         user_answer = get_answer()
@@ -14,9 +15,8 @@ def start_game(game):
             x += 1
             continue
         elif user_answer != correct_answer:
-            print(f"""{user_answer} is wrong answer ;(. \n
-                  Correct answer was {correct_answer}.\n
-                  Let's try again, {name}!""")
+            print(f"""{user_answer} is wrong answer ;(. Correct answer was {correct_answer}.
+Let's try again, {name}!""")
             break
     else:
         print(f'Congratulations, {name}!')
@@ -25,3 +25,10 @@ def start_game(game):
 def get_answer():
     answer = input('Your answer: ')
     return answer
+
+
+def get_name():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return name
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,13 +5,18 @@ EXERCISE = 'What number is missing in the progression?'
 
 
 def generate_question():
+    n, numbers = generate_random()
+    correct_answer = numbers[n]
+    numbers[n] = ".."
+    sequence_numbers = " ".join(map(str, numbers[0:10]))
+    return sequence_numbers, correct_answer
+
+
+def generate_random():
     num1 = randint(1, 10)
     num2 = randint(100, 120)
     n = randint(2, 9)
     numbers = []
     for i in range(num1, num2, n):
         numbers.append(i)
-    correct_answer = numbers[n]
-    numbers[n] = ".."
-    sequence_numbers = " ".join(map(str, numbers[0:10]))
-    return sequence_numbers, correct_answer
+    return n, numbers
